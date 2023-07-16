4193033d62d945720d0f496b932c1aaa0ab50548
ali313ee <138843216+ali313ee@users.noreply.github.com>
2023-07-16 03:08:06 +0300

    Update PY.py

--- PY.py
+++ PY.py
@@ -27,7 +27,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2023, 7, 15, 2, 00 ,0)
+g= datetime.datetime(2023, 7, 17, 2, 00 ,0)
 
 if (x.strftime("%x"))>(g.strftime("%x")):
  print('\n\n')
